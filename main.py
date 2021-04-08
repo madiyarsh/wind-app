5aa24905f960f60050287a6ea5626dd0f312ad5f
Madiyar Sharipov <40263450+madiyarsh@users.noreply.github.com>
2021-04-08 16:26:51 +0600

    Create main.py
    Temp from map

---
+++ main.py
@@ -0,0 +1,22 @@
+import pyowm
+
+owm = pyowm.OWM('a4059161b180b05d8caa7275375bdf1d')
+
+    
+city = 'Almaty'
+
+loc = owm.weather_at_place(city)
+
+weather = loc.get_weather()
+
+    # wind 
+wind = weather.get_wind()
+
+    # temperature
+temp = weather.get_temperature(unit='celsius')
+cleaned_temp_data = (int(temp['temp']))
+
+print 'The temperature in', city ,'is',  cleaned_temp_data, "Celcius Degree"
+print (wind)
+
+
